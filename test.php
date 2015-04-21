280a0588ab667338422ace754484d8b30373ba5e
JackyChung1125 <JackyChung1125@users.noreply.github.com>
2015-04-20 23:37:34 -0400

    Update test.php

--- test.php
+++ test.php
@@ -17,20 +17,36 @@
 	<tr>
 		<td>
 		<form method="post" action="#"> 
-   		First Name:<input type="text" name="first_name" value="<?php echo $first_name;?>">
+   		First Name:<input type="text" name="first_name">
 		<br><br>
-   		Last Name:<input type="text" name="last_name" value="<?php echo $last_name;?>">
+   		Last Name:<input type="text" name="last_name">
    		<br><br>
-   		<input type="submit" name="submit" value="Submit"> 
+   		<input type="submit" value="Submit"> 
+   		<input type="reset" value="Reset"> 
 		</form>
 		</td>
 	</tr>
 <?php
-	echo "<h2>Your Input:</h2>";
-	echo $first_name;
-	echo "<br>";
-	echo $last_name;
-	echo "<br>";
+
+	$db_connection = mysqli_connect("localhost", "Jacky", "0987654321", "baseball");
+
+	echo "<h2>Output:</h2>";
+	$query="select * from master where nameFirst='".$_POST['first_name']."' and nameLast='".$_POST['last_name']."'";
+	$rs=$db_connection->query($query);									
+	$num=$rs->num_rows;
+	
+	
+	while($row=$rs->fetch_assoc()){
+		echo "Given Name: {$row['nameGiven']}<br/>";
+		echo "Birth Date: {$row['birthMonth']}/{$row['birthDay']}/{$row['birthYear']}<br/>";
+		echo "Hometown: {$row['birthCity']}, {$row['birthState']}, {$row['birthCountry']}<br/>";
+		echo "Weight: {$row['weight']}<br/>";
+		echo "Height: {$row['height']}<br/>";
+		echo "Bat Hand: {$row['bats']}<br/>";
+		echo "Throw Hand: {$row['throws']}<br/>";	
+	}
+	
+	mysqli_close($db_connection);
 ?>
 
 <footer>
